3bfed6f0f1ce90119d7a305227ddd012d1e93675
Breno do Val <brenorvale@gmail.com>
2023-11-10 22:26:21 -0300

    feat(BundlerServer): add logic to get bundle based on query

--- bundleServer/BundlerHandler.js
+++ bundleServer/BundlerHandler.js
@@ -1,27 +1,44 @@
 const fs = require('fs');
-const path = require('path')
+const url = require("url");
+const path = require('path');
+
 const { BaseHandler } = require('./BaseHandler');
 
 class BundlerHandler extends BaseHandler {
     handle(request, response) {
-        if (request.url.match(/^repackminiapp|/)) {
-            // build query params
+        const requestUrl = url.parse(request.url);
+        if (requestUrl.pathname === '/repackminiapp') {
+            const [ platform, appVersion] = requestUrl.query.split('&').map(query => {
+                const [ queryKey, querySearch] = query.split('=');
+                return {
+                    [queryKey]: querySearch
+                }
+            });
+
             let bundler = null; 
 
-            fs.readFile(path.resolve(__dirname, './bundlerLocation.json'), (err, data) => {
+            fs.readFile('./bundlerLocation.json', (err, data) => {
                 if (err) {
                     response.writeHead(500, {'Content-Type': 'text/plain'});
                     response.end('internal server error');
                     return null;
                 }
 
-               bundler = data.toString();
+                bundler = JSON.parse(data.toString());
+
+                const apiResponse = bundler[platform['platform']]?.[appVersion['appVersion']]
+
+                if (!apiResponse) {
+                    response.writeHead(404, {'Content-Type': 'text/plain'});
+                    response.end('Not found');
+                    return null;
+                }
+
+                response.writeHead(200, {'Content-Type': 'application/json'});
+
+                response.end(JSON.stringify({ url: apiResponse }));
+                return;
             });
-
-            response.writeHead(200, {'Content-Type': 'text/plain'});
-
-            response.end(JSON.stringify({ response: 'OK'}));
-            return;
         } else {
             return super.handle(request, response);
         }
